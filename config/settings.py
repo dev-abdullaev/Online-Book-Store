09fcb1a51e9182779171d20efff24348e58e0ed6
Zokhid Abdullaev <77606851+dev-abdullaev@users.noreply.github.com>
2021-10-30 00:36:38 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'django.contrib.comments',
     "django.contrib.staticfiles",
     "shop",
     "crispy_forms",
