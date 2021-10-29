c644da7b6374910b813bb8284887bce8ec3e32ee
Zokhid Abdullaev <77606851+dev-abdullaev@users.noreply.github.com>
2021-10-30 00:50:23 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", 'NAME': '/tmp/config.sqlite3',}}
 
 
 AUTH_PASSWORD_VALIDATORS = [
